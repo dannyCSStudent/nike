ba39df95a0588937a660be6990303eee88ad00a6
Dan Villegas <danv8605@gmail.com>
2023-08-23 07:47:40 -0600

    Completed SpecialOffer and CustomerReviews

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 
 
-const Button = ({ label, iconURL}) => {
+const Button = ({ label, iconURL, backGroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${backGroundColor ? `${backGroundColor} ${textColor} ${borderColor}` : "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}>
         {label}
         {iconURL && <img 
             src={iconURL}
